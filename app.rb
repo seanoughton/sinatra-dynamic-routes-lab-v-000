3065514c5e5df71ffdf7a125fdde87a9710c88ab
Sean Oughton <seanoughton@gmail.com>
2018-03-23 20:23:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -37,7 +37,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    @return_string = 
+    @return_string = @number1 @operation @number2
   end
 
 end
